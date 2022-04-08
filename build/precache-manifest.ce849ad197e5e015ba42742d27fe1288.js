d85b37bf7e0cbcc3a10f5d3a279aa2470ef8ef4e
grit-ki <slogup.ki@gmail.com>
2022-04-08 12:25:35 +0900

    feat: csp

---
+++ build/precache-manifest.ce849ad197e5e015ba42742d27fe1288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1db99c50e2896270175d",
+    "url": "/static/css/main.ec03dfb0.chunk.css"
+  },
+  {
+    "revision": "1db99c50e2896270175d",
+    "url": "/static/js/main.1db99c50.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7235baa75d2b1c20a9a4150b714da503",
+    "url": "/index.html"
+  }
+];
